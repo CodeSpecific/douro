8e7f31b878f738dae1894f4d4967f2e02d008c27
wangdi <wangdi@tidebuy.com>
2019-05-09 11:45:51 +0800

    重构用户DAO模型和配合访问方法

--- core/user.go
+++ core/user.go
@@ -15,24 +15,18 @@ type userCore struct {
 
 func (c *userCore) GetUserProfile(id uint) (*entity.User_Profile, error) {
 	user := entity.User_Profile{}
-	has, err := db.Get(&user)
+	_, err := db.Get(&user)
 	if err != nil {
 		return nil, err
-	}
-	if !has {
-		return nil, nil
 	}
 	return &user, nil
 }
 
 func (c *userCore) GetUserAuth(id uint) (*entity.User_Auth, error) {
 	auth := entity.User_Auth{}
-	has, err := db.Get(&auth)
+	_, err := db.Get(&auth)
 	if err != nil {
 		return nil, err
-	}
-	if !has {
-		return nil, nil
 	}
 	return &auth, nil
 }
